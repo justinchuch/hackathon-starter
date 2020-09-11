3367c30e03b67874b8ebea1abfc20dfcebf35034
Justin Chu <justin.chu@datastax.com>
2020-09-11 19:00:47 -0400

    [WIP]

--- app.js
+++ app.js
@@ -3,17 +3,17 @@
  */
 const express = require('express');
 const compression = require('compression');
-const session = require('express-session');
+const session = require('express-session'); //
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const chalk = require('chalk');
 const errorHandler = require('errorhandler');
 const lusca = require('lusca');
 const dotenv = require('dotenv');
-const MongoStore = require('connect-mongo')(session);
+//const MongoStore = require('connect-mongo')(session); //
 const flash = require('express-flash');
 const path = require('path');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose'); //
 const passport = require('passport');
 const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
@@ -47,16 +47,18 @@ const app = express();
 /**
  * Connect to MongoDB.
  */
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGODB_URI);
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-  process.exit();
-});
+//console.log('MONGODB_URI = ', process.env.MONGODB_URI)
+//
+//mongoose.set('useFindAndModify', false);
+//mongoose.set('useCreateIndex', true);
+//mongoose.set('useNewUrlParser', true);
+//mongoose.set('useUnifiedTopology', true);
+//mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connection.on('error', (err) => {
+//  console.error(err);
+//  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+//  process.exit();
+//});
 
 /**
  * Express configuration.
@@ -79,10 +81,10 @@ app.use(session({
   saveUninitialized: true,
   secret: process.env.SESSION_SECRET,
   cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
-  store: new MongoStore({
-    url: process.env.MONGODB_URI,
-    autoReconnect: true,
-  })
+//  store: new MongoStore({
+//    url: process.env.MONGODB_URI,
+//    autoReconnect: true,
+//  })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
--- models/User.js
+++ models/User.js
@@ -1,75 +1,145 @@
+'use strict';
+
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  emailVerificationToken: String,
-  emailVerified: Boolean,
+//const userSchema = new mongoose.Schema({
+//  email: { type: String, unique: true },
+//  password: String,
+//  passwordResetToken: String,
+//  passwordResetExpires: Date,
+//  emailVerificationToken: String,
+//  emailVerified: Boolean,
+//
+//  snapchat: String,
+//  facebook: String,
+//  twitter: String,
+//  google: String,
+//  github: String,
+//  instagram: String,
+//  linkedin: String,
+//  steam: String,
+//  twitch: String,
+//  quickbooks: String,
+//  tokens: Array,
+//
+//  profile: {
+//    name: String,
+//    gender: String,
+//    location: String,
+//    website: String,
+//    picture: String
+//  }
+//}, { timestamps: true });
 
-  snapchat: String,
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
-  steam: String,
-  twitch: String,
-  quickbooks: String,
-  tokens: Array,
+class User {
 
-  profile: {
-    name: String,
-    gender: String,
-    location: String,
-    website: String,
-    picture: String
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
   }
-}, { timestamps: true });
+
+  static findOne() {
+    console.log('findOne');
+    // GET
+    // https://66d701da-d348-43fa-a484-b7eafdea7795-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/hackathon/collections/users/<username>
+    // with header: X-Cassandra-Token
+    return "user";
+  }
+
+  static findById() {
+    console.log('findById');
+    return "user";
+  }
+
+  static deleteOne() {
+    // DELETE
+    console.log('deleteOne');
+  }
+
+  save() {
+    // PUT
+    // https://66d701da-d348-43fa-a484-b7eafdea7795-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/hackathon/collections/users/<username>
+    // with header: X-Cassandra-Token
+    // with header: Content-Type : application/json
+    console.log('save');
+
+  }
+
+//  pre('save', function save(next) {
+//    console.log('pre');
+//    const user = this;
+//    if (!user.isModified('password')) { return next(); }
+//    bcrypt.genSalt(10, (err, salt) => {
+//      if (err) { return next(err); }
+//      bcrypt.hash(user.password, salt, (err, hash) => {
+//        if (err) { return next(err); }
+//        user.password = hash;
+//        next();
+//      });
+//    });
+//  });
+
+  static comparePassword(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      cb(err, isMatch);
+    });
+  };
+
+  static gravatar(size) {
+    console.log('gravatar');
+
+    if (!size) {
+      size = 200;
+    }
+    if (!this.email) {
+      return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+    }
+    const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+    return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+  }
+}
 
 /**
  * Password hash middleware.
  */
-userSchema.pre('save', function save(next) {
-  const user = this;
-  if (!user.isModified('password')) { return next(); }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err); }
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) { return next(err); }
-      user.password = hash;
-      next();
-    });
-  });
-});
+//userSchema.pre('save', function save(next) {
+//  const user = this;
+//  if (!user.isModified('password')) { return next(); }
+//  bcrypt.genSalt(10, (err, salt) => {
+//    if (err) { return next(err); }
+//    bcrypt.hash(user.password, salt, (err, hash) => {
+//      if (err) { return next(err); }
+//      user.password = hash;
+//      next();
+//    });
+//  });
+//});
 
 /**
  * Helper method for validating user's password.
  */
-userSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch);
-  });
-};
+//userSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+//  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+//    cb(err, isMatch);
+//  });
+//};
 
 /**
  * Helper method for getting user's gravatar.
  */
-userSchema.methods.gravatar = function gravatar(size) {
-  if (!size) {
-    size = 200;
-  }
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
+//userSchema.methods.gravatar = function gravatar(size) {
+//  if (!size) {
+//    size = 200;
+//  }
+//  if (!this.email) {
+//    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+//  }
+//  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+//  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+//};
 
-const User = mongoose.model('User', userSchema);
+//const User = mongoose.model('User', userSchema);
 
 module.exports = User;
